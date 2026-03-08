d27a6447487d5212b78e20c37102726f1cf21141
IGFCODES <igocheigoche001@gmail.com>
2026-03-08 09:31:49 +0100

    vercel deployment error resolution

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,8 @@
 import prisma from "@/lib/prisma";
 import Link from "next/link";
 
+export const dynamic = "force-dynamic";
+
 export default async function AdminDashboard() {
   const [userCount, courseCount, enrollmentCount] = await Promise.all([
     prisma.user.count(),
